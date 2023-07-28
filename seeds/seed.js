6673bfc637ae1e8cdada4d436218de06929040e8
hd9 <hectorjr_805@yahoo.com>
2023-07-27 21:13:00 -0700

    added seeds seed file

---
+++ seeds/seed.js
@@ -0,0 +1,25 @@
+const sequelize = require('../config/connection');
+const { User, UserNotes } = require('../models');
+
+const userData = require('./userData.json');
+const noteData = require('./userNotes.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const note of noteData) {
+    await UserNotes.create({
+      ...note,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
