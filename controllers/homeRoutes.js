8cfe105dfac78685ffdeea4a16f38474e89aa227
[Elias Montes] <[eliasmontes911@gmail.com]>
2023-07-27 20:07:48 -0700

    add homeroutes and json dependencies

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+// get route for home page
+router.get("/", async (req, res) => {
+  try {
+    //   res is related to handlebars
+    // if user is already logged in
+    if(req.session.loggedIn) {
+        res.render('homepage');
+    }
+    else {
+    res.render("login");
+}
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+module.exports = router;
