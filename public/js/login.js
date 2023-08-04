7f1f2d11472a530774e868a6ae2289063a32a3e3
[Elias Montes] <[eliasmontes911@gmail.com]>
2023-08-03 22:06:44 -0700

    renamed jss file, changes to handlebars, added to homeroutes

--- public/js/login.js
+++ public/js/login.js
@@ -31,14 +31,14 @@ const signupFormHandler = async (event) => {
   const password = document.querySelector('#password-signup').value.trim();
 
   if (name && email && password) {
-    const response = await fetch('/api/users', {
+    const response = await fetch('/api/user', {
       method: 'POST',
       body: JSON.stringify({ name, email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/note');
     } else {
       alert(response.statusText);
     }
